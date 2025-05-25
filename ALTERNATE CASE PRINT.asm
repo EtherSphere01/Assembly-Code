af60d534ca7749dc1564962405e00260af4c10e7
Md. Naimur Rahman <naimurrahmanprogramming@gmail.com>
2025-05-26 01:19:40 +0600

    '.'

---
+++ ALTERNATE CASE PRINT.asm
@@ -0,0 +1,66 @@
+.MODEL SMALL
+.STACK 100H
+.DATA
+LOW DB 0AH, 0DH,"LOWER CASE IS: $" 
+HIGH DB 0AH, 0DH,"HIGHER CASE ID: $"
+.CODE MAIN
+MAIN PROC
+     MOV AX, @DATA
+     MOV DS, AX
+     
+     INPUT:
+     MOV AH, 01H
+     INT 21H
+     MOV BL, AL
+     
+     CHECK_HIGHER1: 
+     CMP BL, 41H
+     JGE CHECK_HIGHER2
+     JMP CHECK_LOWER1
+     
+     CHECK_HIGHER2:
+     CMP BL, 5AH
+     JLE DISPLAY_LOWER
+     JMP CHECK_LOWER1
+     
+     CHECK_LOWER1:
+     CMP BL, 61H
+     JGE CHECK_LOWER2
+     JMP EXIT
+     
+     CHECK_LOWER2:
+     CMP BL, 7AH
+     JLE DISPLAY_HIGHER
+     JMP EXIT 
+     
+     DISPLAY_LOWER:
+     ADD BL, 20H
+     LEA DX, LOW
+     MOV AH, 09H
+     INT 21H
+     
+     MOV DL, BL
+     MOV AH, 02H
+     INT 21H
+     JMP EXIT
+     
+     DISPLAY_HIGHER:
+     SUB BL, 20H
+     LEA DX, HIGH
+     MOV AH, 09H
+     INT 21H
+     
+     MOV DL, BL
+     MOV AH, 02H
+     INT 21H
+     JMP EXIT 
+     
+    
+EXIT:
+    MOV AH, 4CH
+    INT 21H 
+    
+    
+    
+MAIN ENDP
+END MAIN
